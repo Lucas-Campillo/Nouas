93a02b34bf3bd95017cd60b6493b7a1330c534a3
Lucas_Campillo <lucas.campillo@hotmail.fr>
2020-03-18 16:25:51 +0100

    mise en place d'un system de session

--- models/users.js
+++ models/users.js
@@ -23,6 +23,16 @@ class Users{
         return this.row.admin
     }
 
+    static create(data)
+    {
+        let sql = "INSERT INTO users (username,password) VALUES (?)"
+        DB.query(sql,[data], function (err, result) {
+            if (err) throw err
+            console.log("Insert done")
+            
+          })
+    }
+
     static verify(user, pass, cb , error)
     {
         let sql = "SELECT * FROM users WHERE username = ? AND password = ?"
--- server.js
+++ server.js
@@ -20,59 +20,70 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false }
 }))
-
 // Routes
+    app.get('/', (request, response) =>{
+        response.render('index', { Name : request.session.username , Admin : request.session.admin} )
+    }) 
+    
+    app.post('/', (request, response) =>{
+        response.render('index', {test : 'Salut' , Nom : request.body.name })
+    })
+    
+    app.get('/news', (request,response) =>
+    {
+        console.log(request.session.username)
+        console.log(request.session.admin)
+        news.all(function(news){
+        response.render('news', {News : news, Admin : request.session.admin})
+    })
+    })
+    
+    app.post('/news/create' , (request, response) =>
+    {
+        news.create([request.body.name,request.body.content,new Date()])
+        response.redirect('/news')
+    })
+    
+    app.get('/news/delete/:Id' , (request , response ) =>{
+        news.delete(request.params.Id)
+        response.redirect('/news')
+    })
+    
+    app.post('/news/update/:Id', (request , response ) =>{
+        news.update([request.body.name,request.body.content,new Date()],request.params.Id)
+        response.redirect('/news')
+    })
 
-app.get('/', (request, response) =>{
-    response.render('index', {test: 'Salut'} )
-}) 
+    app.get('/login', (request, response) =>{
+        response.render('login')
+    })
+    app.post('/login' , (request,response)=>
+    {
+        users.verify(request.body.username,request.body.password, function(user){
+            request.session.username = user[0].username
+            request.session.admin = user[0].admin
+            response.redirect('/')
+        },function(err){ response.render('login', { Erreur : err})})
+    })
 
-app.post('/', (request, response) =>{
-    console.log(url.parse(request.url).name)
-    response.render('index', {test : 'Salut' , Nom : request.body.name })
-})
-
-app.get('/news', (request,response) =>
-{
-    console.log(request.session.user)
-    console.log(request.session.admin)
-    news.all(function(news){
-        response.render('news', {News : news})
-})
-})
-
-app.get('/logIn' , (request,response)=>
-{
-    response.render('logIn')
-})
-
-app.post('/login' , (request,response)=>
-{
-    users.verify(request.body.username,request.body.password, function(user){
-    console.log(user[0].admin)
-
-        request.session.user = user[0].username
-        request.session.admin = user[0].admin
-
-        response.render('index', { User : user})
-    },function(err){ response.render('login', { Erreur : err})})
-})
+    app.get('/signin', (request, response) =>{
+        response.render('signin')
+    })
+    app.post('/signin' , (request,response)=>
+    {
+        users.create([request.body.username,request.body.password])
+        users.verify(request.body.username,request.body.password, function(user){
+            request.session.username = user[0].username
+            request.session.admin = user[0].admin
+            response.redirect('/')
+        },function(err){ response.render('login', { Erreur : err})})
+    })
+    app.get('/logout' , (request,response)=>
+    {
+        request.session.destroy();
+       response.redirect('/login')
+    })
 
 
-app.post('/news/create' , (request, response) =>
-{
-    news.create([request.body.name,request.body.content,new Date()])
-    response.redirect('/news')
-})
-
-app.get('/news/delete/:Id' , (request , response ) =>{
-    news.delete(request.params.Id)
-    response.redirect('/news')
-})
-
-app.post('/news/update/:Id', (request , response ) =>{
-    news.update([request.body.name,request.body.content,new Date()],request.params.Id)
-    response.redirect('/news')
-})
 
 app.listen(8080)
